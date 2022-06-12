6aa4258340fea081f1e80c7494ffd8cb0aa21e60
Bailey Pearson <bailey.pearson@gmail.com>
2022-06-12 10:04:23 -0400

    feat: add logging to chorebot
    - chore: add pino and remove morgan logging libraries
    - feat: add basic logging to app
    - chore: improve development script with pino-pretty

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@ import { Client, Intents, CacheType, Interaction } from "discord.js";
 import * as dotenv from "dotenv";
 import { buildCommand } from "./commands";
 import { once } from "events";
+import { pino } from "pino";
 
+const logLevel = process.env.LOG_LEVEL ?? "info";
+const logger = pino({ level: logLevel });
 dotenv.config();
 
 const token = process.env.DISCORD_TOKEN;
@@ -11,11 +14,13 @@ const token = process.env.DISCORD_TOKEN;
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 async function initialize(client: Client, token: string): Promise<string> {
+  logger.debug("Initializing client...");
   const [_, string] = await Promise.all([
     once(client, "ready"),
     client.login(token),
   ]);
 
+  logger.debug("Successfully initialized client.");
   return string;
 }
 
@@ -34,11 +39,20 @@ async function main() {
   for await (const interaction of interactionCreateEvents(client)) {
     const command = buildCommand(interaction);
     if (command != null) {
+      logger.debug(`Processing command ${command.commandName}`);
       try {
         await command.execute(interaction);
+        logger.debug(`Successfully executed ${command.commandName}`);
       } catch (err) {
         await interaction.reply("oops, something went wrong");
+        logger.debug(`Something went wrong processing ${command.commandName}`);
       }
+    } else {
+      logger.error(
+        undefined,
+        "Received unknown command",
+        interaction.commandName
+      );
     }
   }
 }
